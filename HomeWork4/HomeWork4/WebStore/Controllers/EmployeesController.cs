434a538cdbfbb48ac0faf0c280c95ab5c656060a
Yulia <jbachareva@gmail.com>
2021-06-07 09:36:31 +0600

    Homework4

---
+++ HomeWork4/HomeWork4/WebStore/Controllers/EmployeesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Data;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.Models;
+using WebStore.ViewModels;
+
+namespace WebStore.Controllers
+{
+    //[Route("NewRoute/[controller]/123")]
+    //[Route("Staff")]
+    public class EmployeesController : Controller
+    {
+        private readonly IEmployeesData _EmployeesData;
+
+        public EmployeesController(IEmployeesData EmployeesData)
+        {
+            _EmployeesData = EmployeesData;
+        }
+
+        //[Route("List")]
+        public IActionResult Index() => View(_EmployeesData.Get());
+
+        //[Route("{id}")]
+        public IActionResult EmployeeDetails(int id)
+        {
+            var employee = _EmployeesData.GetById(id);
+            if (employee is null)
+                return NotFound();
+
+            return View(employee);
+        }
+
+        #region Редактирование
+
+        public IActionResult Edit(int? Id)
+        {
+            if (Id is null) return View(new EmployeeViewModel());
+
+            if (Id < 0)
+                return BadRequest();
+
+            var employee = _EmployeesData.GetById((int)Id);
+            if (employee is null)
+                return NotFound();
+
+            return View(new EmployeeViewModel
+            {
+                Id = employee.Id,
+                Surname = employee.Surname,
+                Name = employee.FirstName,
+                Patronymic = employee.Patronymic,
+                Age = employee.Age
+            });
+        }
+
+        [HttpPost]
+        public IActionResult Edit(EmployeeViewModel Model)
+        {
+            if (Model is null)
+                throw new ArgumentNullException(nameof(Model));
+
+            if(Model.Age < 18 || Model.Age > 75)
+                ModelState.AddModelError("Age", "Сотрудник не проходит по возрасту");
+
+            if(Model.Name == "123" && Model.Surname == "QWE")
+                ModelState.AddModelError(string.Empty, "Странное сочетание имени и фамилии");
+
+            if (!ModelState.IsValid)
+                return View(Model);
+
+            var employee = new Employee
+            {
+                Id = Model.Id,
+                FirstName = Model.Name,
+                Surname = Model.Surname,
+                Patronymic = Model.Patronymic,
+                Age = Model.Age
+            };
+
+            if (Model.Id == 0)
+                _EmployeesData.Add(employee);
+            else
+                _EmployeesData.Edit(employee);
+
+            _EmployeesData.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        #endregion
+
+        #region Удаление
+
+        public IActionResult Delete(int id)
+        {
+            if (id <= 0)
+                return BadRequest();
+
+            var employee = _EmployeesData.GetById(id);
+            if (employee is null)
+                return NotFound();
+
+            return View(new EmployeeViewModel
+            {
+                Id = employee.Id,
+                Surname = employee.Surname,
+                Name = employee.FirstName,
+                Patronymic = employee.Patronymic,
+                Age = employee.Age
+            });
+        }
+
+        [HttpPost]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            _EmployeesData.Delete(id);
+            _EmployeesData.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        #endregion
+    }
+}
---
+++ HomeWork4/HomeWork4/WebStore/Startup.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileSystemGlobbing;
+using Microsoft.Extensions.Hosting;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.Infrastructure.Services;
+
+namespace WebStore
+{
+    public class Startup
+    {
+        private IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews(opt =>
+                {   
+                    //opt.Filters.Add<>()
+                    //opt.Conventions
+                    //opt.Conventions.Add();
+                })
+               .AddRazorRuntimeCompilation();
+
+            services.AddSingleton<IEmployeesData, InMemoryEmployeesData>();
+            //services.AddTransient<IEmployeesData, InMemoryEmployeesData>();
+            //services.AddScoped<IEmployeesData, InMemoryEmployeesData>();
+
+            services.AddSingleton<IProductData, InMemoryProductData>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env/*, IServiceProvider Services*/)
+        {
+            //var employees = Services.GetService<IEmployeesData>();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+
+            app.UseStaticFiles();
+            app.UseDefaultFiles();
+
+            app.UseWelcomePage("/MVC");
+
+            //app.Use(async (context, next) =>
+            //{
+            //    Debug.WriteLine($"Request to {context.Request.Path}");
+            //    await next(); // Можем прервать конвейер не вызывая await next()
+            //    // постобработка
+            //});
+            //app.UseMiddleware<>()
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}"
+                    );
+            });
+        }
+    }
+}
