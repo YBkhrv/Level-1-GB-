61e790c895451f746ad1b39711568c27c2b11bd8
Yulia <jbachareva@gmail.com>
2021-06-15 19:39:08 +0600

    Homework7

---
+++ HomeWork7/HomeWork7/WebStore.DAL/Context/WebStoreDB.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebStore.Domain.Entities;
+using WebStore.Domain.Entities.Employees;
+using WebStore.Domain.Entities.Identity;
+
+namespace WebStore.DAL.Context
+{
+    public class WebStoreDB : IdentityDbContext<User, Role, string>
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Section> Sections { get; set; }
+
+        public DbSet<Brand> Brands { get; set; }
+
+        public DbSet<Employee> Employees { get; set; }
+        
+        public WebStoreDB(DbContextOptions<WebStoreDB> Options) : base(Options) { }
+    }
+}
---
+++ HomeWork7/HomeWork7/WebStore.Domain/Entities/Identity/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebStore.Domain.Entities.Identity
+{
+    public class User : IdentityUser
+    {
+        public const string Administrator = "Admin";
+        public const string DefaultAdminPassword = "AdminPаssword";
+    }
+}
